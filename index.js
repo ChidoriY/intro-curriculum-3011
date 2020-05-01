e23ab29ffc774431e72f8808584dae9aac579e8f
ChidoriY <y.chidori.266@gmail.com>
2020-05-01 09:46:45 +0900

    HTML を表示するよう変更

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(`<!DOCTYPE html>
+  <html lang="ja">
+  <head>
+      <meta charset="UTF-8">
+      <title>Document</title>
+  </head>
+  <body>
+      <h1>HTMLの一番大きい見出しを表示します</h1>
+  </body>
+  </html>`); // ``での文字列指定は改行にも対応しているためそのまま貼り付けられる
   res.end();
 });
 const port = 8000;
